cec1f1716c5dd88b1b208a80161b159cc875a451
Lesley Easley <lesleyeasley@Lesleys-MacBook-Air.local>
2021-08-19 21:07:43 -0500

    aug 19

--- scripts/app_trader_LDE.sql
+++ scripts/app_trader_LDE.sql
@@ -26,3 +26,87 @@ FROM play_store_apps;
 
 SELECT DISTINCT category
 FROM play_store_apps;
+
+SELECT *
+FROM play_store_apps JOIN app_store_apps
+ON play_store_apps.rating = app_store_apps.rating;
+
+ALTER TABLE play_store_apps DROP COLUMN category;
+
+SELECT * 
+FROM play_store_apps;
+
+ALTER TABLE play_store_apps DROP COLUMN install_count;
+
+SELECT *
+FROM play_store_apps JOIN app_store_apps
+ON play_store_apps.rating = app_store_apps.rating;
+
+SELECT rating
+FROM play_store_apps
+UNION ALL 
+SELECT rating
+FROM app_store_apps;
+
+SELECT COUNT(*)
+FROM play_store_apps JOIN app_store_apps
+ON play_store_apps.rating = app_store_apps.rating;
+
+ALTER TABLE app_store_apps DROP COLUMN currency;
+
+
+SELECT *
+FROM play_store_apps JOIN app_store_apps
+ON play_store_apps.rating = app_store_apps.rating;
+
+ALTER TABLE app_store_apps ALTER COLUMN review_count TYPE INTEGER  USING (review_count::integer);
+
+SELECT name, rating, review_count
+FROM play_store_apps 
+UNION ALL 
+SELECT name,rating, review_count
+FROM app_store_apps;
+
+ALTER TABLE app_store_apps
+RENAME COLUMN primary_genre TO genres;
+
+SELECT name, rating, review_count,genres
+FROM play_store_apps 
+UNION ALL 
+SELECT name,rating, review_count,genres
+FROM app_store_apps;
+
+SELECT name,rating, review_count, genres
+FROM play_store_apps;
+UNION ALL
+SELECT name,rating, review_count, genres
+FROM app_store_apps
+WHERE rating > 4;
+
+SELECT name,rating, review_count, genres
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC;
+
+SELECT name,rating,review_count,genres
+FROM play_store_apps
+UNION ALL
+SELECT name,rating,review_count,genres
+FROM app_store_apps
+WHERE rating > 4
+ORDER BY review_count DESC;
+
+SELECT name, rating, review_count, genres
+FROM app_store_apps
+WHERE rating >4 
+ORDER BY review_count DESC
+LIMIT 20;
+
+SELECT SUM(review_count)
+FROM play_store_apps;
+
+SELECT name, SUM(review_count),genres
+FROM play_store_apps
+GROUP BY name, SUM(review_count),
+GROUP BY SUM(review_count) DESC
+LIMIT 20;
