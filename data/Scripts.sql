3ded421758ed3bc8fabfd1b7e241149566c3c113
ojackson00110 <ojackson00110@gmail.com>
2021-08-21 13:47:51 -0500

    Saturday Progress

--- data/Scripts.sql
+++ data/Scripts.sql
@@ -10,9 +10,27 @@ from app_store_apps;
 select *
 from play_store_apps;
 
-select max(rating) AS rating, name AS app_name, review_count
+SELECT distinct name AS app_name, max(rating) AS rating, review_count, price, type
 FROM play_store_apps
-where rating is not null
-	AND review_count >1000
-group by review_count, app_name
-order by rating desc;
+WHERE rating is not null
+	AND review_count >=100000
+	AND rating >= 4.5
+	AND type = 'Free'
+GROUP BY review_count, app_name, price, type
+ORDER BY rating desc;
+
+SELECT *
+FROM app_store_apps;
+
+ALTER TABLE app_store_apps ALTER COLUMN review_count TYPE INTEGER  USING (review_count::integer);
+ALTER TABLE app_store_apps ALTER COLUMN price TYPE INTEGER  USING (price::integer);
+
+SELECT name, price, review_count, rating, primary_genre
+FROM app_store_apps
+WHERE review_count >=100000
+	AND rating >= 4.0
+	AND price <=1
+	AND primary_genre = 'Games'
+GROUP BY review_count, name, price, rating, primary_genre
+ORDER BY rating desc;
+
