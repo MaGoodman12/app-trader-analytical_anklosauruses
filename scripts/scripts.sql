2317467e83419193661fa8a788a428fbc6e34ed6
MaGoodman12 <magoody12@gmail.com>
2021-08-22 21:22:07 -0500

    Prepared_for_visuals

--- scripts/scripts.sql
+++ scripts/scripts.sql
@@ -6,96 +6,70 @@
 	*/
 --select * from app_store_apps;
 --select * from play_store_apps;
---alter table __ alter column __ TYPE INTEGER  USING (__::integer);
+--alter table __ alter column __ TYPE __  USING (__::__);
 
 
---APP_Genre_&_genre_count
-/*SELECT
-	distinct primary_genre,
-	count(primary_genre) AS dist_genre
-FROM (
+--Genre_recommendation by rating and count of review
+--APP
+SELECT
+	distinct primary_genre as APP_dist_genre,
+	count(primary_genre) AS genre_count
+FROM
+(	
 	SELECT name,price,review_count,rating,primary_genre
 	FROM app_store_apps
+	WHERE rating >= 4.5
+	and review_count >= 100000
 	GROUP BY name,price,review_count,rating,primary_genre
-	ORDER BY rating desc, primary_genre asc) AS sub1
-GROUP BY primary_genre
-ORDER BY dist_genre desc;
+	ORDER BY rating desc, primary_genre asc
+	limit 25
+) AS sub1
+GROUP BY APP_dist_genre
+ORDER BY genre_count desc;
 
---Relevant_app_columns
-SELECT name,price,review_count,rating,primary_genre
-FROM app_store_apps
-GROUP BY name,price,review_count,rating,primary_genre
-ORDER BY rating desc, primary_genre asc;
+--PLAY
+SELECT
+	distinct lower(replace(category,'FAMILY', 'KIDS_GAME')) as play_category,
+	count(category) AS cat_count
+FROM
+(
+	SELECT name,price,review_count,rating,category
+	FROM play_store_apps
+	WHERE rating >= 4.5
+	and review_count >= 100000
+	GROUP BY name,price,review_count,rating,category
+	ORDER BY rating desc, category asc
+	limit 25
+) AS sub_2
+GROUP BY play_category
+ORDER BY cat_count desc;
 
 --Working_end_result
-ALTER TABLE play_store_apps ALTER COLUMN price TYPE numeric USING (price::numeric);
 ALTER TABLE app_store_apps ALTER COLUMN review_count TYPE numeric USING (review_count::numeric);
-
-WITH app AS
-(
-SELECT
-	name AS app_name, 
-	price, 
-	round(avg(review_count),1) AS avg_a_rev, 
-	rating,
-	primary_genre
-FROM app_store_apps
-WHERE rating >= 4.5
-GROUP BY
-	app_name, 
-	price, 
-	review_count, 
-	rating,
-	primary_genre
-ORDER BY avg_a_rev desc, rating desc
-LIMIT 25
-),
-
-play AS
-(
-SELECT
-	name,
-	price,
-	rating,
-	round(avg(review_count),1) as avg_p_rev,
-	genres
-FROM play_store_apps
-WHERE rating >= 4.5
-group by
-	name, 
-	price, 
-	rating,
-	genres
-ORDER BY
-	avg_p_rev desc,
-	rating desc
-LIMIT 25
-)*/
 
 with app AS
 (SELECT
+ 	distinct name as dist_name,
 	primary_genre as genres,
-	name,
 	rating,
-	price,
+	cast(price as text) as price,
 	round(avg(review_count),1) as avg_a_rev
 FROM app_store_apps
 WHERE rating >= 4.5 AND review_count >=100000
 group by
 	genres,
-	name,
+	dist_name,
 	rating,
 	price
 ORDER BY
 	avg_a_rev desc,
 	rating desc
-LIMIT 25
 ),
 
 play AS
 (SELECT
+ 	distinct name as dist_name,
 	genres,
-	name,
 	rating,
 	replace(price, '0', '0.00') as price,
 	round(avg(review_count),1) as avg_p_rev
@@ -111,12 +85,19 @@ group by
 ORDER BY
 	avg_p_rev desc,
 	rating desc
-LIMIT 25
 )
 
-select *
+select app.genres,
+	app.dist_name,
+	app.rating, 
+	app.price, 
+	round(sum(avg_a_rev + avg_p_rev),1) AS reviews
 from app
-union all
-select *
-from play
-order by name asc;
+JOIN play
+on app.dist_name = play.dist_name and app.price = play.price 
+group by app.genres, app.dist_name, app.rating, app.price
+order by reviews desc
+limit 25;
+
+--testing
+
